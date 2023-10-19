59654ed5e618b05d751136f8db76796f665eaf90
Camila Parise Dias <116179030+Camila-Parise-Dias@users.noreply.github.com>
2023-10-19 09:37:55 -0300

    Updated 01_visao_empresa.py

--- pages/01_visao_empresa.py
+++ pages/01_visao_empresa.py
@@ -168,9 +168,10 @@ st.sidebar.markdown( '## Selecione uma data limite' )
 
 date_slider = st.sidebar.slider( 
     'Até qual valor?',
-        value=pd.to_datetime( '2022-4-6', yearfirst= False),
-    min_value=pd.to_datetime('2022-2-11', yearfirst= False),
-    max_value=pd.to_datetime( '2022-4-6',yearfirst= False))
+        value=pd.to_datetime( '2022-4-6', yearfirst=False),
+    min_value=pd.to_datetime('2022-2-11', yearfirst=False),
+    max_value=pd.to_datetime( '2022-4-6',yearfirst=False),
+    format=None)
 
 st.sidebar.markdown( """---""" )
 
